b0507e274f207ed149dac8a261ba068147682ef3
salsdev <salehaliyu111@gmail.com>
2024-02-05 23:58:46 +0000

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+module for lookup.
+"""
+
+
+def lookup(obj):
+    """Return the attrs"""
+    return dir(obj)
+
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+module for BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+    pass
+
